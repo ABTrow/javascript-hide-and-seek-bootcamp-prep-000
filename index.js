7861f8263eb89960e0f5951fcab583eded004d52
Austin Trow <austintrow@gmail.com>
2019-06-19 20:52:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let listItems = document.querySelectorAll('.ranked-list li');
+  for (let item of listItems) {
+    item.innerHTML = parseInt(item.innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let current = document.querySelector('div#grand-node');
+  let next = []
+  while (current.children.length !== 0) {
+    current = current.children[0];
+  }
+  return current;
+}
