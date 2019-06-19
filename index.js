585f9bb5dd5a3456f134ae52693a1b4919702e51
Austin Trow <austintrow@gmail.com>
2019-06-19 20:37:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-/
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
 function nestedTarget() {
-  document.querySelector('#nested.target');
+  return document.querySelector('#nested.target');
 }
 
